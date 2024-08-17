315545e6c530730912f414095bcf883574588238
Randa Mopobela <98214133+randamopobela@users.noreply.github.com>
2024-08-18 05:09:20 +0900

    add experience tab dan perbaikan minor

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,28 +3,29 @@ module.exports = {
 content: ["./public/**/*.{html,js}"],
 theme: {
     extend: {
-    container: {
-        center: true
-    },
+        container: {
+            center: true
+        },
 
-    fontFamily: {
-        k2d: ['K2D'],
-        lato: ['Lato']
-    },
-    
-    colors: {
-        'porto-bg': '#121212',
-        'light': '#E7F8FF',
-        'light-hover': '#D8F3FF',
-        'light-active': '#AFE7FF',
-        'normal': '#00B1FD',
-        'normal-hover': '#0076E4',
-        'normal-active': '#006ACD',
-        'dark': '#0063C0',
-        'dark-hover': '#004F99',
-        'dark-active': '#003B72',
-        'darker': '#002E59',
-    }
+        fontFamily: {
+            k2d: ['K2D'],
+            lato: ['Lato']
+        },
+        
+        colors: {
+            'porto-bg': '#121212',
+            'porto-darker': '#1B1B1B',
+            'light': '#E7F8FF',
+            'light-hover': '#D8F3FF',
+            'light-active': '#AFE7FF',
+            'normal': '#00B1FD',
+            'normal-hover': '#0076E4',
+            'normal-active': '#006ACD',
+            'dark': '#0063C0',
+            'dark-hover': '#004F99',
+            'dark-active': '#003B72',
+            'darker': '#002E59',
+        }
     },
 },
 plugins: [],
